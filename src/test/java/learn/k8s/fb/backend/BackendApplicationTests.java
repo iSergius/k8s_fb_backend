1d4a5304dbdd1e63b92f95b5d4199eebfe1bc4d8
Sergey Kondratyev <SKondratev@bellintegrator.com>
2021-11-11 15:56:03 +0500

    Init commit

---
+++ src/test/java/learn/k8s/fb/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.k8s.fb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
